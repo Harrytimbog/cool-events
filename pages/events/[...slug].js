cf7545e42e9cd91cd87069b864233dee5065b350
Timilehin Arigbede <Harriemannie@gmail.com>
2022-06-03 15:34:44 +0100

    Reusing Logic inside A component

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -43,15 +43,38 @@ function FilteredEventsPage(props) {
     }
   }, [data]);
 
+  let pageHeadData = <Head>
+    <title>Filtered Events</title>
+    <meta 
+      name='description' 
+      content={`A list of filtered events.`} 
+    />
+  </Head>
+  
   if (!loadedEvents) {
-    return <p className='center'>Loading...</p>;
+    return (
+      <Fragment>
+        {pageHeadData}
+        <p className='center'>Loading...</p>
+      </Fragment>
+    );
   }
-
+  
   const filteredYear = filterData[0];
   const filteredMonth = filterData[1];
-
+  
   const numYear = +filteredYear;
   const numMonth = +filteredMonth;
+  
+  pageHeadData = (
+    <Head>
+      <title>Filtered Events</title>
+      <meta 
+        name='description' 
+        content={`All events for ${numMonth}/${numYear}.`} 
+      />
+    </Head>
+  );
 
   if (
     isNaN(numYear) ||
@@ -64,6 +87,7 @@ function FilteredEventsPage(props) {
   ) {
     return (
       <Fragment>
+        {pageHeadData}
         <ErrorAlert>
           <p>Invalid filter. Please adjust your values!</p>
         </ErrorAlert>
@@ -85,6 +109,7 @@ function FilteredEventsPage(props) {
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
       <Fragment>
+        {pageHeadData}
         <ErrorAlert>
           <p>No events found for the chosen filter!</p>
         </ErrorAlert>
@@ -99,13 +124,7 @@ function FilteredEventsPage(props) {
 
   return (
     <Fragment>
-      <Head>
-        <title>Filtered Events</title>
-        <meta 
-          name='description' 
-          content={`All events for ${numMonth}/${numYear}.`} 
-        />
-      </Head>
+      {pageHeadData}
       <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
     </Fragment>
