a3a10a181dabfe6a6e7a8073c82d5809d8bfc4c3
Timilehin Arigbede <Harriemannie@gmail.com>
2022-06-04 21:37:24 +0100

    Adding Error handling to server-side

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,19 @@
 import { MongoClient } from 'mongodb';
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://harrie:0WPQTtlaSNMs22iH@cluster0.5t4ke.mongodb.net/cool-events?retryWrites=true&w=majority'
+  );
+
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+
+  await db.collection('newsletter').insertOne(document);
+}
+
 const handler = async (req, res) => {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
@@ -9,14 +23,24 @@ const handler = async (req, res) => {
       return;
     }
 
-    const client = await MongoClient.connect(
-      'mongodb+srv://harrie:0WPQTtlaSNMs22iH@cluster0.5t4ke.mongodb.net/cool-events?retryWrites=true&w=majority'
-    );
-    const db = client.db();
+    let client;
 
-    await db.collection('newsletter').insertOne({ email: userEmail });
+    try {
+      client = await connectDatabase(); 
+    } catch (error) {
+      res.status(500).json({message: 'Connecting to database failed!'});
+      return;
+    }
+
+    try {   
+      await insertDocument(client, { email: userEmail })
+      client.close();
+    } catch (error) {
+      res.status(500).json({message: 'Inserting data into database failed!'});
+      return;
+    }
+
   
-    client.close();
 
     res.status(201).json({ message: 'Signed up!' });
   }
